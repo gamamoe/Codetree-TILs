be706f50cc7819981118e487d043e2bafb28eaeb
Geunho Lee <lkh7689@gmail.com>
2025-01-09 19:39:32 +0900

    Title: 알파벳에 따른 평가, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250109/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,15 @@
+import sys
+
+char = sys.stdin.readline().rstrip()
+if char == 'S':
+    print('Superior')
+elif char == 'A':
+    print('Excellent')
+elif char == 'B':
+    print('Good')
+elif char == 'C':
+    print('Usually')
+elif char == 'D':
+    print('Effort')
+else:
+    print('Failure')
