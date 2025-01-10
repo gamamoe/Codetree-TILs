93167ccf5a467926ae55fcc730abfaa4261b01d7
Geunho Lee <lkh7689@gmail.com>
2025-01-10 19:51:30 +0900

    Title: 특정 조건 세 정수 비교, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 250110/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
+++ 250110/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -2,4 +2,4 @@ import sys
 
 a, b, c = [int(x) for x in sys.stdin.readline().split()]
 
-print(f'{1 if a <= b and a <= c else 0}'" "f'{1 if a == b and b == c else 0}')
+print(f'{1 if a <= b and a <= c else 0} {1 if a == b and b == c else 0}')
