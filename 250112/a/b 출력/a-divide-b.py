926ec5c3f7f808292f18e148ac14a36f77b81539
Geunho Lee <lkh7689@gmail.com>
2025-01-12 18:43:55 +0900

    Title: a/b 출력, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250112/a/b 출력/a-divide-b.py
+++ 250112/a/b 출력/a-divide-b.py
@@ -3,12 +3,12 @@ import sys
 a, b = [int(x) for x in sys.stdin.readline().split()]
 answer = [f'{a // b}.']
 
+if a >= b:
+    a %= b
+
 for _ in range(20):
-    if a < b:
-        a *= 10
-        answer.append(str(a // b))
-        a %= b
-    else:
-        a %= b
+    a *= 10
+    answer.append(str(a // b))
+    a %= b
 
 print(''.join(answer))
