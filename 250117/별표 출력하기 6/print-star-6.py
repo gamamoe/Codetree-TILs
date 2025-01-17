d21b81eee2c732232e8e684bfd8fa9eddecf8617
Geunho Lee <lkh7689@gmail.com>
2025-01-17 19:33:21 +0900

    Title: 별표 출력하기 6, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250117/별표 출력하기 6/print-star-6.py
@@ -0,0 +1,15 @@
+import sys
+
+n = int(sys.stdin.readline())
+answer = []
+for i in range(n, 0, -1):
+    space = ' ' * (2 * (n - i))
+    stars = ' '.join('*' for _ in range(2 * i - 1))
+    answer.append(f'{space}{stars}')
+
+for i in range(2, n + 1):
+    space = ' ' * (2 * (n - i))
+    stars = ' '.join('*' for _ in range(2 * i - 1))
+    answer.append(f'{space}{stars}')
+
+print('\n'.join(answer))
