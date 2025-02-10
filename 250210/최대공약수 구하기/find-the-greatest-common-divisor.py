e9bdb4ad91f55e6b1f828fe897aa28764697bf1d
Geunho Lee <lkh7689@gmail.com>
2025-02-10 20:25:24 +0900

    Title: 최대공약수 구하기, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250210/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 250210/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -1,16 +1,14 @@
 import sys
 
-def gcd(n: int, m: int) -> int:
-    if n > m:
-        return gcd(m, n)
-    
+def gcd(m: int, n: int) -> int:
+    if m < n:
+        m, n = n, m
     if n == 0:
         return m
-    
     if m % n == 0:
         return n
     else:
-        return gcd(m, m % n)
+        return gcd(n, m%n)
 
 n, m = [int(x) for x in sys.stdin.readline().split()]
 print(gcd(n, m))
