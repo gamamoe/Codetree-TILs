f74f6318f05c3fd67af8a46dd32e81fb84d745d7
Geunho Lee <lkh7689@gmail.com>
2025-02-12 18:25:26 +0900

    Title: 함수를 이용한 소수 판별, Time: 50ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250212/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 250212/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -10,6 +10,7 @@ def is_prime(n: int) -> bool:
 a, b = [int(x) for x in sys.stdin.readline().split()]
 answer = 0
 for num in range(a, b + 1):
-    answer += num if is_prime(num) else 0
+    if is_prime(num):
+        answer += num
 
 print(answer)
