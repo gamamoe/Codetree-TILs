db70e83ba7a5697962a5c1ee7a57765b7515d017
Geunho Lee <lkh7689@gmail.com>
2025-02-21 19:16:51 +0900

    Title: 문자열 계속 지우기, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250221/문자열 계속 지우기/keep-removing-string.py
+++ 250221/문자열 계속 지우기/keep-removing-string.py
@@ -5,15 +5,17 @@ b = sys.stdin.readline().rstrip()
 stack = []
 
 len_of_b = len(b)
-for char in a:
-    if not stack:
-        stack.append(char)
-        continue
+if len_of_b == 1:
+    print(''.join([x for x in a if x != b]))
+else:
+    for char in a:
+        if not stack:
+            stack.append(char)
+            continue
 
-    if f'{"".join(stack[-(len_of_b - 1):])}{char}' == b:
-        for _ in range(len_of_b - 1):
-            stack.pop()
-    else:
-        stack.append(char)
-
-print(''.join(stack))
+        if f'{"".join(stack[-(len_of_b - 1):])}{char}' == b:
+            for _ in range(len_of_b - 1):
+                stack.pop()
+        else:
+            stack.append(char)
+    print(''.join(stack))
